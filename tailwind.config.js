059a386753d0dd08d83bc6cbe80f13740166cd26
VISHESH2022 <124697163+VISHESH2022@users.noreply.github.com>
2023-11-02 00:41:34 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'garamond': ['Garamond', 'serif'],
+      },
+      colors: {
+        headingcolor: "#E2C275",
+        ochre: "#bd974b",
+        Cinnamon: "#D2691E",
+        SaddleBrown: "#8B4513",
+        Sienna: "#A0522D",
+        almond: "#EADDCA",
+        brass: "#E1C16E",
+        darkbrass: "#dec27d",
+        CafeNoir: "#4B3621",
+        WoodBrown: "#C19A6B",
+        NavyBlue: "#000080",
+        DeepGray: "#333333",
+        White: "#FFFFFF",
+        DeepRed: "#800000",
+        Gold: "#D4AF37",
+        ForestGreen: "#228B22",
+        Silver: "#C0C0C0",
+        Black: "#000000",
+        LightBlue: "#00CED1",
+        Cream: "#FFFFF0",
+
+        bakerchocolate: "#422311",
+        twine: "#C18A54",
+        buff: "#EE964D",
+        seabuckthorn: "#F0D274",
+
+
+        
+      },
+    },
+  },
+  plugins: [],
+}
+
